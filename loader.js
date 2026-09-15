32eb2eb9cc885bffcd0e998bd523985d81b39d15
8k9fgd7kf7-art <8k9fgd7kf7@privaterelay.appleid.com>
2026-09-15 17:38:59 +0200

    Commit-Pin des Loaders korrigieren

--- loader.js
+++ loader.js
@@ -6,7 +6,7 @@ window.SNIPE_HELPER_CONFIG = {
     breite: null
 };
 
-$.getScript("https://cdn.jsdelivr.net/gh/8k9fgd7kf7-art/SnipeHelper@5270841594be4b2f0b3af544449c3807be35d44e6/snipe-helper.js")
+$.getScript("https://cdn.jsdelivr.net/gh/8k9fgd7kf7-art/SnipeHelper@5270841594be4b2f0b3af5443df8e777b3a17d34/snipe-helper.js")
     .fail(function () {
         if (window.UI?.ErrorMessage) {
             UI.ErrorMessage("Der Snipe-Helfer konnte nicht geladen werden.");
