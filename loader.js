66095d588073e614ee63db2485dda3ddd83a0975
8k9fgd7kf7-art <8k9fgd7kf7@privaterelay.appleid.com>
2026-09-15 17:38:50 +0200

    Loader fest auf Snipe-Helfer 2.0.1 pinnen

--- loader.js
+++ loader.js
@@ -6,7 +6,7 @@ window.SNIPE_HELPER_CONFIG = {
     breite: null
 };
 
-$.getScript("https://cdn.jsdelivr.net/gh/8k9fgd7kf7-art/SnipeHelper@main/snipe-helper.js?v=2.0.1")
+$.getScript("https://cdn.jsdelivr.net/gh/8k9fgd7kf7-art/SnipeHelper@5270841594be4b2f0b3af544449c3807be35d44e6/snipe-helper.js")
     .fail(function () {
         if (window.UI?.ErrorMessage) {
             UI.ErrorMessage("Der Snipe-Helfer konnte nicht geladen werden.");
